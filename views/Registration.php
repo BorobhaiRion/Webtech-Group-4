05c3f837c3f5b3d08d024f786490648947ba3d40
Asfaq <asfaqfahad@gmail.com>
2026-05-15 02:34:52 +0600

    updated

--- views/Registration.php
+++ views/Registration.php
@@ -3,11 +3,24 @@ include '../config/helpers.php';
 include 'header.php';
 ?>
 
+<div class="auth-form" style="max-width: 500px; margin: 20px auto;">
+    <table class="layout-table">
+        <tr>
+            <td style="background: #eeeeee;"><h2>Register</h2></td>
+        </tr>
+        <tr>
+            <td>
+                <form action="../controllers/RegisterController.php" method="POST">
+                    <table class="form-table">
 
 
 
 
-
-
+                     </table>
+                </form>    
+            </td>
+        </tr>
+  </table>
+</div>
 
 <?php include 'footer.php'; ?>
