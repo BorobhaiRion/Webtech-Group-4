5f62ecf0372e9e17c3fc91991ab4c85fa716c656
Asfaq <asfaqfahad@gmail.com>
2026-05-15 02:57:54 +0600

    Reg page designed

--- views/Registration.php
+++ views/Registration.php
@@ -12,12 +12,32 @@ include 'header.php';
             <td>
                 <form action="../controllers/RegisterController.php" method="POST">
                     <table class="form-table">
+                        
+                        <tr>
+                            <td width="150"><label>Name</label></td>
+                            <td><input type="text" name="name" required></td>
+                        </tr>
+                        <tr>
+                            <td><label>Email</label></td>
+                            <td><input type="email" name="email" required></td>
+                        </tr>
+                        <tr>
+                            <td><label>Phone (Optional)</label></td>
+                            <td><input type="text" name="phone"></td>
+                        </tr>
+                        <tr>
+                            <td><label>Password</label></td>
+                            <td><input type="password" name="password" required minlength="8"></td>
+                        </tr>
+                        <tr>
+                            <td></td>
+                            <td><button type="submit" class="btn">Register</button></td>
+                        </tr>     
+                                
 
-
-
-
+                        
                      </table>
-                </form>    
+                </form>
             </td>
         </tr>
   </table>
