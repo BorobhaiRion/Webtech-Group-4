086ec24539d729570d2c7209d2312dd4ded9f4fc
Asfaq <asfaqfahad@gmail.com>
2026-05-16 01:47:41 +0600

    layout set

--- views/register.php
+++ views/register.php
@@ -4,10 +4,28 @@ include 'header.php';
 ?>
 
 
+<div class="auth-form" style="max-width: 500px; margin: 20px auto;">
+    <table class="layout-table">
+        <tr>
+            <td style="background: #eeeeee;"><h2>Register</h2></td>
+        </tr>
+        <tr>
+            <td>
+                <form action="../controllers/RegisterController.php" method="POST">
+                    <table class="form-table">
 
 
 
 
+
+
+
+                    </table>
+                </form>
+            </td>
+        </tr>
+    </table>
+</div>
 
 
 <?php include 'footer.php'; ?>
