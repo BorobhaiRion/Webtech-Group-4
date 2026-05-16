4640013071d40e94d391b9cbc2d29bcb332d2329
Asfaq <asfaqfahad@gmail.com>
2026-05-16 10:47:19 +0600

    validation updated

--- controller/RegisterController.php
+++ controller/RegisterController.php
@@ -2,7 +2,24 @@
 require_once '../config/helpers.php';
 require_once '../models/db.php';
 
+$name = "";
+$email = "";
+$phone = "";
+$password = "";
+
+if($_SERVER["REQUEST_METHOD"]=="POST"){
+    $name = validateInput($_POST["name"]);
+    $email = validateInput($_POST["email"]);
+    $phone = validateInput($_POST["phone"]);
+    $password = $_POST["password"];
 
 
+
+    
+}
+else{
+        setFlash('error', implode("<br>", $errors));
+        redirect('../views/register.php');
+    }
 
 ?>
